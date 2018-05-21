c11a315bd58614e189c6cb070629841381ae5963
lumaquan <cogane7955@gmail.com>
2018-05-21 10:43:55 -0500

    MainActivityFragment register Sensor accelerometer.

--- app/src/main/java/com/example/luismauricio/doodlzz/MainActivity.java
+++ app/src/main/java/com/example/luismauricio/doodlzz/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.luismauricio.doodlzz;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,9 +24,61 @@ public class MainActivity extends LifecycleActivity {
         setContentView(R.layout.just_another);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Resources resources = getResources();
+        Configuration configuration = resources.getConfiguration();
+        int screenSize = configuration.screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
+        int screenOrientation = configuration.orientation;
+
+        Log.d("TAG", "density: " + configuration.densityDpi);
+        Log.d("TAG", "width: " + configuration.screenWidthDp);
+        Log.d("TAG", "height: " + configuration.screenHeightDp);
+        Log.d("TAG", "smallest width: " + configuration.smallestScreenWidthDp);
+
+
+        switch (screenSize){
+            case Configuration.SCREENLAYOUT_SIZE_SMALL:
+             toastMessage("small");
+            break;
+            case Configuration.SCREENLAYOUT_SIZE_NORMAL:
+                toastMessage("normal");
+                break;
+            case Configuration.SCREENLAYOUT_SIZE_LARGE:
+                toastMessage("large");
+                break;
+            case Configuration.SCREENLAYOUT_SIZE_XLARGE:
+                toastMessage("xlarge");
+                break;
+            case Configuration.SCREENLAYOUT_SIZE_UNDEFINED:
+                toastMessage("undefinied");
+                break;
+        }
+
+        switch (screenOrientation){
+            case Configuration.ORIENTATION_PORTRAIT:
+                toastMessage("portrait");
+                break;
+            case Configuration.ORIENTATION_LANDSCAPE:
+                toastMessage("landscape");
+                break;
+        }
+
+
+        if(screenSize == Configuration.SCREENLAYOUT_SIZE_NORMAL){
+               setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }else{
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+
     }
 
     public void onOn(View view) {
         Toast.makeText(this, "Second version UP", Toast.LENGTH_SHORT).show();
     }
+
+
+    private void toastMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
 }
--- app/src/main/java/com/example/luismauricio/doodlzz/MainActivityFragment.java
+++ app/src/main/java/com/example/luismauricio/doodlzz/MainActivityFragment.java
@@ -1,11 +1,13 @@
 package com.example.luismauricio.doodlzz;
 
 import android.annotation.SuppressLint;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -15,6 +17,27 @@ import android.view.ViewGroup;
 public class MainActivityFragment extends LifecycleFragment {
 
     String name = getClass().getSimpleName();
+
+    private DoodleView doodleView;
+    private float acceleration;
+    private float currentAcceleration;
+    private float lastAcceleration;
+    private boolean dialogOnScreen;
+
+    private static final int ACCELERATION_THRESHOLD = 100000;
+    private static final int SAVE_IMAGE_PERMISSION_REQUEST_CODE = 1;
+
+    private SensorEventListener sensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
 
     public MainActivityFragment() {
     }
@@ -29,8 +52,25 @@ public class MainActivityFragment extends LifecycleFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        doodleView = view.findViewById(R.id.doodleView);
+        acceleration = 0.00f;
+        currentAcceleration = SensorManager.GRAVITY_EARTH;
+        lastAcceleration = SensorManager.GRAVITY_EARTH;
+        return view;
     }
+
+     private void enableAccelerometerListening(){
+        SensorManager sensorManager = (SensorManager)getActivity().getSystemService(Context.SENSOR_SERVICE);
+       sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+
+     }
+
+     private void disableAccelerometerListening(){
+         SensorManager sensorManager = (SensorManager)getActivity().getSystemService(Context.SENSOR_SERVICE);
+         sensorManager.unregisterListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+
+     }
 
 
     @Override
